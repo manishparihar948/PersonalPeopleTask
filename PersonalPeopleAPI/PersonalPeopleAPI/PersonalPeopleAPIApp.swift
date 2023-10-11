ea16ee086829347c1ea27c10f83b269419c90043
manishparihar948 <manish.parihar948@gmail.com>
2023-10-11 22:23:47 +0200

    Initial project setup

---
+++ PersonalPeopleAPI/PersonalPeopleAPI/PersonalPeopleAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalPeopleAPIApp.swift
+//  PersonalPeopleAPI
+//
+//  Created by Manish Parihar on 11.10.23.
+//
+
+import SwiftUI
+
+@main
+struct PersonalPeopleAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

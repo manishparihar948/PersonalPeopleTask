b724614b364199027725e6bb7f8ee76387df6c1f
manishparihar948 <manish.parihar948@gmail.com>
2023-10-11 22:36:30 +0200

    Added Resources with Symbol and Theme

---
+++ PersonalPeopleAPI/PersonalPeopleAPI/Resources/Symbols.swift
@@ -0,0 +1,17 @@
+//
+//  Symbols.swift
+//  PersonalPeopleAPI
+//
+//  Created by Manish Parihar on 11.10.23.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
+
+
+
---
+++ PersonalPeopleAPI/PersonalPeopleAPI/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  PersonalPeopleAPI
+//
+//  Created by Manish Parihar on 11.10.23.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let pill = Color("pill")
+}

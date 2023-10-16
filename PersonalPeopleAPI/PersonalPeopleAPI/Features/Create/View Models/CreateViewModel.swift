097d2f6d6253205b5ecb4ad147bf789b700144c8
manishparihar948 <manish.parihar948@gmail.com>
2023-10-16 22:55:24 +0200

    Added CreateValidator and Easy form in CreateView

--- PersonalPeopleAPI/PersonalPeopleAPI/Features/Create/View Models/CreateViewModel.swift
+++ PersonalPeopleAPI/PersonalPeopleAPI/Features/Create/View Models/CreateViewModel.swift
@@ -14,33 +14,49 @@ final class CreateViewModel: ObservableObject {
     // via a binding
     @Published var person = NewPerson()
     @Published private(set) var state: SubmissionState?
-    @Published private(set) var error: NetworkingManager.NetworkingError?
+    @Published private(set) var error: FormError?
     @Published var hasError = false
+    
+    // Create instance of createvalidator
+    private let validator = CreateValidator()
     
     func create() {
         
-        // current state of data
-        state = .submitting
-        
-        let encoder = JSONEncoder()
-        encoder.keyEncodingStrategy = .convertToSnakeCase
-        let data = try? encoder.encode(person)
-        
-        NetworkingManager
-            .shared
-            .request(methodType: .POST(data: data), "https://reqres.in/api/users?delay=3") { [weak self] res in
-                // Result get from the service on to the main thread in UI
-                DispatchQueue.main.async {
-                    switch res {
-                    case .success:
-                        self?.state = .successful
-                    case .failure(let error):
-                        self?.state = .unsuccessful
-                        self?.hasError = true
-                        self?.error = error as? NetworkingManager.NetworkingError
+        do {
+            //after validate
+            try validator.validate(person)
+            
+            // current state of data
+            state = .submitting
+            
+            let encoder = JSONEncoder()
+            encoder.keyEncodingStrategy = .convertToSnakeCase
+            let data = try? encoder.encode(person)
+            
+            NetworkingManager
+                .shared
+                .request(methodType: .POST(data: data), "https://reqres.in/api/users?delay=3") { [weak self] res in
+                    // Result get from the service on to the main thread in UI
+                    DispatchQueue.main.async {
+                        switch res {
+                        case .success:
+                            self?.state = .successful
+                        case .failure(let err):
+                            self?.state = .unsuccessful
+                            self?.hasError = true
+                            if let networkingError = err as? NetworkingManager.NetworkingError {
+                                self?.error = .networking(error: networkingError )
+                            }
+                        }
                     }
                 }
+            
+        } catch {
+            self.hasError = true
+            if let validationError = error as? CreateValidator.CreateValidatorError {
+                self.error = .validation(error: validationError)
             }
+        }
     }
 }
 
@@ -49,5 +65,23 @@ extension CreateViewModel {
         case unsuccessful
         case successful
         case submitting
+    }
+}
+
+extension CreateViewModel {
+    enum FormError: LocalizedError {
+        case networking(error: LocalizedError)
+        case validation(error: LocalizedError)
+    }
+}
+
+// To check which type of error is in our view present, network type or validation type
+extension CreateViewModel.FormError {
+    var errorDescription: String? {
+        switch self {
+        case .networking(let err),
+             .validation(let err):
+            return err.errorDescription
+        }
     }
 }
--- PersonalPeopleAPI/PersonalPeopleAPI/Features/Create/Views/CreateView.swift
+++ PersonalPeopleAPI/PersonalPeopleAPI/Features/Create/Views/CreateView.swift
@@ -11,15 +11,25 @@ struct CreateView: View {
     
     // Done button Dismiss
     @Environment(\.dismiss) var dismiss
+    @FocusState private var focusedField: Field? // to make textfield not active
     @StateObject private var vm = CreateViewModel()
     let successfulAction: () -> Void // Create closure
     
     var body: some View {
         NavigationStack {
             Form {
-                firstname
-                lastname
-                job
+                
+                Section {
+                    firstname
+                    lastname
+                    job
+                } footer: {
+                    if case .validation(let err) = vm.error,
+                       let errorDesc = err.errorDescription {
+                        Text(errorDesc)
+                            .foregroundStyle(.red)
+                    }
+                }
                 
                 Section {
                     submit
@@ -51,6 +61,14 @@ struct CreateView: View {
     }
 }
 
+extension CreateView {
+    enum Field: Hashable {
+        case firstName
+        case lastName
+        case job
+    }
+}
+
 #Preview {
     CreateView {}
 }
@@ -65,19 +83,24 @@ private extension CreateView {
     
     var firstname: some View {
         TextField("First Name",text: $vm.person.firstName)
+            .focused($focusedField, equals: .firstName)
     }
     
     var lastname: some View {
         TextField("Last Name",text: $vm.person.lastName)
+            .focused($focusedField, equals: .lastName)
+
     }
     
     var job: some View {
         TextField("Job",text: $vm.person.job)
+            .focused($focusedField, equals: .job)
+
     }
     
     var submit: some View {
         Button("Submit") {
-            // TODO: Handle action
+            focusedField = nil // resign textfield above create function , resolve removing in log also
             vm.create()
         }
     }
